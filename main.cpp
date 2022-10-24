c4da08c8f7dc08b7ac7af618f75a0d63afa09dfb
Dunix <taiTragedija@gmail.com>
2022-10-24 23:19:44 +0300

    Added Quick Sort

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <random>
 #include <algorithm>
 
-#define n 100 // number of elements to be sorted
-#define scale 4 // scale of elements on screen
-#define delay 5 // delay between every comparison
+#define n 100 // number of elements to be sorted (3 - ~{SCREEN_RESOLUTION_HEIGHT}/scale)
+#define scale 4 // scale of elements on screen (1 - inf)
+#define delay 5 // delay in ms between every comparison (0 - inf)
 
 void draw_state(std::vector<int>& v, SDL_Renderer* renderer, unsigned int a, unsigned int b){
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -23,13 +23,37 @@ void draw_state(std::vector<int>& v, SDL_Renderer* renderer, unsigned int a, uns
     SDL_Delay(delay);
 }
 
-void bubble_sort(std::vector<int> v, SDL_Renderer* renderer){
+void bubble_sort(std::vector<int>& v, SDL_Renderer* renderer){
     for(int i = 0; i < n - 1; i++){
         for(int j = i + 1; j < n; j++){
             if(v[i] > v[j])
                 std::swap(v[i],v[j]);
             draw_state(v, renderer, i, j);
         }
+    }
+}
+
+int partition(std::vector<int>& v, SDL_Renderer* renderer, int low, int high){
+    int pivot = v[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++){
+        if(v[j] < pivot){
+            i++;
+            std::swap(v[i], v[j]);
+        }
+        draw_state(v, renderer, i, j);
+    }
+    std::swap(v[i+1], v[high]);
+    draw_state(v, renderer, i+1, high);
+    return i + 1;
+}
+
+void quick_sort(std::vector<int>& v, SDL_Renderer* renderer, int low, int high){
+    int pi;
+    if(low < high){
+        pi = partition(v, renderer, low, high);
+        quick_sort(v, renderer, low, pi-1);
+        quick_sort(v, renderer, pi+1, high);
     }
 }
 
@@ -49,6 +73,9 @@ int main(int argc, char* args[]) {
 
     // Sorting algorithm - bubble sort
     bubble_sort(v, renderer);
+
+    // Sorting algorithm - quick sort
+    quick_sort(v, renderer, 0, n);
 
     return 0;
 }
